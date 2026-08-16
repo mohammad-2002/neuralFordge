c0d1cbe08d62fe584e8f9edc8a4b7fc27d16d534
Mohammad Katwara <solidray64@gmail.com>
2026-08-16 12:39:22 +0530

    fix: format Facebook Pixel tracking script for better readability

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,22 +32,30 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         {process.env.NODE_ENV === 'production' && (
           <>
             <Script id="meta-pixel" strategy="afterInteractive">
-{`!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1288176433723499');
-fbq('track', 'PageView');`}
-            </Script>
-            <noscript>
-              <img height="1" width="1" style={{ display: 'none' }}
-                src="https://www.facebook.com/tr?id=1288176433723499&ev=PageView&noscript=1"
-                alt="meta pixel" />
-            </noscript>
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+
+              fbq('init', '1288176433732499');
+              fbq('track', 'PageView');
+            `}
+          </Script>
+
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src="https://www.facebook.com/tr?id=1288176433732499&ev=PageView&noscript=1"
+              alt="meta pixel"
+            />
+          </noscript>
           </>
         )}
         {children}
